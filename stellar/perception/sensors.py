9588790f37b7c56815dd2f29ae29e7734d001b57
strfx <claudio.pilotti@bluewin.ch>
2020-03-05 11:33:01 +0100

    Added example code to decode data from UART.

--- stellar/perception/sensors.py
+++ stellar/perception/sensors.py
@@ -5,18 +5,13 @@ The tinyK22 sends bundled sensor data over an UART serial interface.
 These messages are demodulated here into distinct values.
 """
 import struct
-import numpy as np
 
 
-class TinyK(object):
-    """
-    Dummy object for testing.
-    """
-
-    def process_blob(self, blob: bytes):
+def decode_blob(blob: bytes):
+    """Decode sensor data array from tinyK22."""
+    try:
         return struct.unpack("fff", blob)
-
-
-    def process(self) -> np.ndarray:
-        """Process an incoming message."""
-        return np.array([0.1, 0.1, 0.0])
+    except struct.error:
+        import binascii
+        error_blob = binascii.hexlify(bytearray(blob))
+        raise ValueError(f"Unable to decode data, got: {error_blob}")
--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -5,27 +5,23 @@ import struct
 import pytest
 import numpy as np
 
-from stellar.perception.sensors import TinyK
+
+from stellar.perception.sensors import decode_blob
 
 
-@pytest.fixture
-def tinyk():
-    # More setup code here
-    return TinyK()
-
-
-def test_should_process_sensor_array(tinyk):
-    """
-    This is just a dummy test.
-    """
-    values = tinyk.process()
-    assert isinstance(values, np.ndarray)
-
-
-def test_decode_sensor_data(tinyk):
+def test_decode_sensor_data():
     sampled_sensor_data = [0.3, 0.2, 0.1]
     blob = struct.pack("fff", *sampled_sensor_data)
 
     expected = (0.3, 0.2, 0.1)
+    got = decode_blob(blob)
+    assert got == pytest.approx(expected, 0.001)
 
-    assert tinyk.process_blob(blob) == pytest.approx(expected, 0.001)
+def test_should_raise_error_on_invald_data():
+    sampled_sensor_data = [0.3, 0.2, 0.1]
+    blob = struct.pack("ddd", *sampled_sensor_data)
+
+    with pytest.raises(ValueError):
+        print(decode_blob(blob))
+    print(decode_blob(blob))
+    assert False
