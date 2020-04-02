988257d1442bd55ce1c4b150a69f0df4f3b64a63
Léonard Wyrsch <leonard.wyrsch@stud.hslu.ch>
2020-04-02 12:39:29 +0200

    add sockets (WIP)

--- stellar/observatory/server.py
+++ stellar/observatory/server.py
@@ -1,13 +1,17 @@
 import json
 import os.path
 import pathlib
+import socket
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from threading import Thread
 
 hostName = 'localhost'
 serverPort = 8080
 
+debug_data = None
 
-class Server(BaseHTTPRequestHandler):
+
+class RequestHandler(BaseHTTPRequestHandler):
     rootPath = os.path.join(pathlib.Path(__file__).parent, 'debug-ui')
 
     routes = {
@@ -41,6 +45,8 @@ class Server(BaseHTTPRequestHandler):
             bytes(json.dumps(self.get_debug_data(), default=str), 'utf-8'))
 
     def get_debug_data(self):
+        return debug_data
+
         import random
         import datetime
         return {
@@ -70,7 +76,27 @@ class Server(BaseHTTPRequestHandler):
         }
 
 
+def listen_to_socket():
+    # AF_UNIX is a lightweight method for interprocess communication,
+    # but it is only available on UNIX systems.
+    # For testing on Windows, we can use INET as alternative.
+    socket_address_family = getattr(socket, 'AF_UNIX', socket.AF_INET)
+    address = (
+        '', 55555) if socket_address_family == socket.AF_INET else 'tmp/stellar/debug'
+
+    listener_socket = socket.socket(socket_address_family, socket.SOCK_STREAM)
+    listener_socket.bind(address)
+    listener_socket.listen()
+
+    while True:
+        client_socket, _ = listener_socket.accept()
+        debug_data = client_socket.recv(1024)
+        client_socket.close()
+
+
 if __name__ == '__main__':
-    with ThreadingHTTPServer((hostName, serverPort), Server) as server:
+    Thread(target=listen_to_socket).start()
+
+    with ThreadingHTTPServer((hostName, serverPort), RequestHandler) as server:
         print('Server listening on port: {}'.format(serverPort))
         server.serve_forever()
