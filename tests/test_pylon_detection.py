05c41aac6c0d0b95220d5e3d349893b89a675eed
Wyrsch Léonard <leonard.wyrsch@stud.hslu.ch>
2020-06-05 12:06:40 +0200

    add video creation scripts and code

--- tests/test_pylon_detection.py
+++ tests/test_pylon_detection.py
@@ -55,3 +55,29 @@ def test_distance_is_80():
     PylonDetector.write_image_debug(image_out, "final")
 
     assert pylons_found[0].estimated_distance_cm == pytest.approx(80, 0.1)
+
+
+@pytest.mark.skip(reason="Primarily used to create pylon recognition proof-of-concept video")
+def test_picam_simulator():
+    """
+    Ensures PicamSimulator works
+    """
+
+    from stellar.perception.picam_simulator import PicamSimulator
+    picam_sim = PicamSimulator("stellar/perception/cv_video_final.mp4")
+
+    is_test_running = True
+    frame_count = 0
+    while is_test_running:
+        has_frame, current_frame = picam_sim.get_next_frame()
+
+        if has_frame:
+            frame_count += 1
+
+            pylons_found = PylonDetector.find_pylons(current_frame)
+            image_out = PylonDetector.mark_pylons(current_frame, pylons_found)
+            PylonDetector.write_image_debug(image_out, "final")
+
+        is_test_running = has_frame
+
+    assert frame_count == 742
